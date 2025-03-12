39b271886e47a78a8ade58b8d81ca8ab355d2e4a
keshava7259 <keshavajagali1182@gmail.com>
2025-03-12 16:13:38 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,24 @@
+// Java Program to Display Numbers
+// from 1 to N Using For Loop and
+// sum of First N Natural Number
+import java.io.*;
+
+// Driver Class
+class GFG {
+      // main function
+    public static void main(String[] args)
+    {
+        int N = 10;
+        int sum = 0;
+        System.out.println("Finding Sum of " + N + " numbers using for loop");
+
+        // we initialize the value of the variable i 
+        // with 1 and increment each time with 1
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+      
+        System.out.println("Sum of first " + N
+                           + " Natural Number = " + sum);
+    }
+}
